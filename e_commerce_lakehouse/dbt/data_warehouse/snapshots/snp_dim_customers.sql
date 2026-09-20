3cf70262ffe196e30cc567ceb5a45c63940b99bb
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2026-09-19 21:16:51 -0300

    fix: adjust snapshots

--- e_commerce_lakehouse/dbt/data_warehouse/snapshots/snp_dim_customers.sql
+++ e_commerce_lakehouse/dbt/data_warehouse/snapshots/snp_dim_customers.sql
@@ -1,19 +1,19 @@
 {% snapshot snap_dim_customers %}
- 
+
 {{
     config(
         target_schema='snapshots',
         unique_key='customer_unique_id',
         strategy='check',
-        check_cols=['city', 'state'],
+        check_cols=['customer_city', 'customer_state'],
     )
 }}
- 
+
 select
     customer_unique_id,
     customer_zip_code_prefix,
     customer_city,
     customer_state
 from {{ source('silver', 'customers') }}
- 
+
 {% endsnapshot %}
